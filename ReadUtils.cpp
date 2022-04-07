2e99e10bfb44a692c2e8fb23f851353e4773aba5
KaiSLOAT <>
2022-04-07 16:27:00 +0000

    Yay! I am done

--- ReadUtils.cpp
+++ ReadUtils.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include "ReadUtils.h"
+enum{MAX_CHARACTERS = 100};
 using namespace std;
 
 void commonErrorCode(const char prompt[]) {
@@ -42,3 +44,32 @@ bool readBool(const char prompt[]){
     return temp == 'Y';
 }
 
+bool compareCharArray(char cArray1[], char cArray2[]){
+  for(int index = 0; index < MAX_CHARACTERS; ++index){
+    if (cArray1[index] != cArray2[index]){
+      return false;
+    }
+  }
+  return true;
+}
+
+void charArrayInput(char array[], string prompt){
+  char temp = '\0';
+  do{
+    cout << prompt;
+    cin >> temp;
+    cin.getline(array, MAX_CHARACTERS - 1, '\n');
+    if(!cin){
+      cin.clear();
+      cin.ignore(9999, '\n');
+      cout << "\n\tInvalid input data.\n" << endl;
+    }
+    else{
+      for(int index = (MAX_CHARACTERS - 1); index > 0; --index){
+        array[index] = array[index - 1];
+      }
+      array[0] = temp;
+      break;
+    }
+  }while(true);
+}
--- ReadUtils.h
+++ ReadUtils.h
@@ -1,3 +1,4 @@
+using namespace std;
 // Pass in the prompt, it will spin until the user enters an integer
 int readInt(const char prompt[]);
 // Pass in the prompt, it will spin until the user enters a double
@@ -5,3 +6,7 @@ double readDouble(const char prompt[]);
 // Ask a y/n question returns true if user enters Y/y or false if user enters N/n
 // It will spin until the user enters a y/n or Y/N
 bool readBool(const char prompt[]);
+//compares 2 character arrays; returns true if they are the same, false if they are different
+bool compareCharArray(char cArray1[], char cArray2[]);
+//this function takes the user input and stores it into a character array (I made this a function when I was already nearly done, so it is not used in places that it could have been had I made it earlier)
+void charArrayInput(char array[], string prompt);
